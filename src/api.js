81581c092b0d6657e65fa8bfa9da6321b33091b6
effect082 <your-email@example.com>
2026-05-18 16:20:03 +0900

    fix(api): update GAS url to apply latest admin logic

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // Google Apps Script Web App URL
-export const GAS_URL = 'https://script.google.com/macros/s/AKfycbwqyrgSHul5mAXgqqj2QJuEr31d6yHRMWCfvnQi9GDOYLnyCPcU9XQIw3c8pkIPxQrr/exec';
+export const GAS_URL = 'https://script.google.com/macros/s/AKfycbzz2fiH45-TMcAJQSugE8xeljQPt5B1-12B30PGvxcnDHBPHgQYQ2i4tA1vtXtaFjy1/exec';
 
 export const fetchGAS = (action, payload = {}) => {
   return new Promise((resolve, reject) => {
